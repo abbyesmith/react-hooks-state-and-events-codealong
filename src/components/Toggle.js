639ce19773251071fc0e6c2a9902df48b4f4dcab
Abby <abby.e.smith.85@gmail.com>
2023-02-21 15:46:38 -0700

    Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
-}
+  const [isOn, setIsOn] = useState(false);
+  //starting with false so the button begins as 'OFF'
+  //useState in the hook with the array []
 
+  
+  function handleClick() {
+      setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style = {{background: color}} onClick = {handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  )
+}
 export default Toggle;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+function Toggle() {
+    const [isOn, setIsOn] = useState(false);
+    //starting with false so the button begins as 'OFF'
+    //useState in the hook with the array []
+
+    
+    function handleClick() {
+        setIsOn((isOn) => !isOn);
+    }
+
+    return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+    //if the state is true, then ON will be on the button. Otherwise, it will read OFF
+}
